aae7ee9def58fbf34bcef41aecc8b62a5d94d806
Ashwath29 <78354532+Ashwath29@users.noreply.github.com>
2022-02-06 20:46:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function addUser() {
+    window.location = "grade.html";
+}
+function addUser1() {
+    window.location = "1to3.html";
+}
+function addUser2() {
+    window.location = "4to6.html";
+}
+function addUser3() {
+    window.location = "7to9.html";
+}
+function addUser4() {
+    window.location = "10to12.html";
+}
